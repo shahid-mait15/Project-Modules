14355cf9123ba67e9e246094b414ed9c7271b46c
shahid-mait15 <118314132+shahid-mait15@users.noreply.github.com>
2023-01-20 20:34:44 +0530

    Add files via upload

---
+++ nat-gateway./variable.tf
@@ -0,0 +1,14 @@
+variable "vpc_id" {}
+variable "internet_gateway" {}
+
+variable "public_subnet_az1_id" {}
+variable "public_subnet_az2_id" {}
+variable "public_subnet_az3_id" {}
+
+variable "private_app_subnet_az1_id" {}
+variable "private_app_subnet_az2_id" {}
+variable "private_app_subnet_az3_id" {}
+
+variable "private_data_subnet_az1_id" {}
+variable "private_data_subnet_az2_id" {}
+variable "private_data_subnet_az3_id" {}
