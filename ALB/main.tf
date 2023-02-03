1b33f1c959d24e061866e079acf7b4692952fec6
shahid-mait15 <118314132+shahid-mait15@users.noreply.github.com>
2023-02-03 18:26:05 +0530

    Add files via upload

--- ALB/main.tf
+++ ALB/main.tf
@@ -1,1 +1,78 @@
+#create application load balancer
+resource "aws_lb" "application_load_balancer" {
+  name               = "${var.project_name}-ALB"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [var.alb_security_group_id]
+  subnets            = [var.public_subnet_az1_id, var.public_subnet_az2_id, var.public_subnet_az3_id]
+
+  enable_deletion_protection = false
+
+ tags = {
+    Name = "${var.project_name}-ALB"
+  }
+}
+
+#create target group
+resource "aws_lb_target_group" "alb_target_group" {
+  name = "${var.project_name}-TG"
+  target_type = "instance"
+  port = 80
+  protocol = "HTTP"
+  vpc_id = var.vpc_id
+
+  health_check{
+    enabled = true
+    interval = 300
+    path = "/"
+    timeout = 60
+    matcher = 200
+    healthy_threshold = 10
+    unhealthy_threshold = 10
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+#create a listener on port 80 
+resource "aws_lb_listener" "alb_http_listener" {
+  load_balancer_arn = aws_lb.application_load_balancer.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_target_group.arn
+  }
+}
+
+resource "aws_lb_target_group_attachment" "target_attachment1" {
+  
+  target_group_arn = aws_lb_target_group.alb_target_group.arn
+  target_id = var.public_instance_az1_id
+  
+  }
+
+resource "aws_lb_target_group_attachment" "target_attachment2" {
+  
+  target_group_arn = aws_lb_target_group.alb_target_group.arn
+  target_id = var.public_instance_az2_id
+  
+  }
+
+
+resource "aws_lb_target_group_attachment" "target_attachment3" {
+  
+  target_group_arn = aws_lb_target_group.alb_target_group.arn
+  target_id = var.public_instance_az3_id
+  
+  }
+
+
+
+
+
+
 
---
+++ ALB/variable.tf
@@ -0,0 +1,13 @@
+variable "project_name" {}
+
+variable "vpc_id" {}
+
+variable "alb_security_group_id" {}
+
+variable "public_subnet_az1_id" {}
+variable "public_subnet_az2_id" {}
+variable "public_subnet_az3_id" {}
+
+variable "public_instance_az1_id" {}
+variable "public_instance_az2_id" {}
+variable "public_instance_az3_id" {}
